3da52b93065dc7b38b214ed222f3e034e894a8ed
Hernan Felipe Cote Martinez <hernan.cote@outlook.com>
2016-04-12 18:29:27 -0500

    presaAmbulancias

--- SistemaDeAmbulancias/src/co/edu/javeriana/ambulancias/negocio/EmpresaAmbulancias.java
+++ SistemaDeAmbulancias/src/co/edu/javeriana/ambulancias/negocio/EmpresaAmbulancias.java
@@ -405,7 +405,7 @@ public class EmpresaAmbulancias implements IServiciosAmbulancias
 	}
 	
 	/**
-	 * Terminate an Asigned service
+	 * Terminate an Assigned service
 	 * @param codigo
 	 * @return
 	 */
